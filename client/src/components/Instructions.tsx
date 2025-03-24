1eb64dea6097f863231908e5499865c913ebcfe3
rymayerhofer <rmayerho@charlotte.edu>
2025-03-23 22:23:08 -0400

    Update Instructions.tsx

--- client/src/components/Instructions.tsx
+++ client/src/components/Instructions.tsx
@@ -1,4 +1,4 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/Accordion";
 
 const Instructions = () => {
   return (
